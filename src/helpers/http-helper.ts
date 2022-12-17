d4411919266633d2e04006ff936d67ec4ef570a3
Victor-Tilve <ing.victortilve@gmail.com>
2022-12-17 17:18:01 -0600

    feat: helper files

---
+++ src/helpers/http-helper.ts
@@ -0,0 +1,17 @@
+import { ServerError } from '../errors/server-error'
+import { HttpResponse } from '../interfaces/http-interface'
+
+export const success = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack as string)
+})

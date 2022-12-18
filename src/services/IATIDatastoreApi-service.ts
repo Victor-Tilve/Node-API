c3e7eac376f7f4f3831213f6bd4f22ac609ce32b
Victor-Tilve <ing.victortilve@gmail.com>
2022-12-17 19:16:54 -0600

    refactor: returning formatter file for the first transactions 10 rows

--- src/services/IATIDatastoreApi-service.ts
+++ src/services/IATIDatastoreApi-service.ts
@@ -10,34 +10,36 @@ export class IatiDatastoreApiService {
 
   async getData (params: IQueryParams): Promise<MonetaryAidResponse> {
     console.log('IatiDatastoreApiService::getData: inside')
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    let _monetaryAidResponse = {} as MonetaryAidResponse
-    _monetaryAidResponse = {
-      2011: {
-        Sida: 181469583
-      },
-      2010: {
-        Sida: 149667518,
-        UD: 6105000
-      },
-      2009: {
-        Sida: 122295311,
-        UD: 30291000
-      },
-      2008: {
-        Sida: 128969145,
-        UD: 33851000,
-        'Folke Bernadotte Academy': 173000,
-        'Svenska institutet': 125000
-      },
-      2007: {
-        Sida: 101561481,
-        UD: 7399000,
-        'Folke Bernadotte Academy': 6000
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions, prefer-const
+    let monetaryAidResponse: MonetaryAidResponse = {}
+    const dataResponse = await this.iatiDatastoreApiRepository.fetchData(params)
+    const docs = dataResponse.response?.docs
+    console.log('iatiDatastoreApi::getMonetaryAid::docs: ' + JSON.stringify(docs))
+    if (docs) {
+      for (let index = 0; index < docs.length; index++) {
+        const date = docs[index].transaction_value_value_date ?? undefined
+        const year = date !== undefined ? new Date(date[0]).getFullYear() : undefined
+        const providerOrg = docs[index].transaction_provider_org_narrative?.[0] ?? undefined
+        const transactionValue = docs[index].transaction_value?.[0] ?? undefined
+        console.log('IatiDatastoreApiService::getData::year: ' + JSON.stringify(year))
+        console.log('IatiDatastoreApiService::getData::providerOrg: ' + JSON.stringify(providerOrg))
+        console.log('IatiDatastoreApiService::getData::transactionValue: ' + JSON.stringify(transactionValue))
+
+        // FIXME: I need to make the currency conversion
+        if (year !== undefined && providerOrg !== undefined && transactionValue !== undefined) {
+          if (providerOrg !== undefined) {
+            if (monetaryAidResponse[year]?.[providerOrg] !== undefined) {
+              monetaryAidResponse[year][providerOrg] = Number(monetaryAidResponse[year][providerOrg]) + Number(transactionValue)
+            } else {
+              // COMEBACK: Check if there is a better way
+              monetaryAidResponse[year] = { ...monetaryAidResponse[year], [providerOrg]: Number(transactionValue) }
+            }
+          }
+        }
       }
     }
-    const dataResponse = await this.iatiDatastoreApiRepository.fetchData(params)
-    console.log('iatiDatastoreApi::getMonetaryAid::dataResponse: ' + JSON.stringify(dataResponse))
-    return _monetaryAidResponse
+
+    console.log('iatiDatastoreApi::getMonetaryAid::monetaryAidResponse: ' + JSON.stringify(monetaryAidResponse))
+    return monetaryAidResponse
   }
 }

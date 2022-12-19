b84c2f5dca20d118ef2b62b788cf46a8cb36d56f
Victor-Tilve <ing.victortilve@gmail.com>
2022-12-19 01:03:19 -0600

    refactor: cleaning server file.  "database connection" deleted

--- src/main/server/server.ts
+++ src/main/server/server.ts
@@ -1,5 +1,4 @@
 import app from '../config/app'
-import ConnectRedis from '../../infra/caching/redis/redis-connect'
 // COMEBACK: SignUp missing
 // COMEBACK: Login missing
 // COMEBACK: user authentication missing
@@ -8,5 +7,4 @@ import ConnectRedis from '../../infra/caching/redis/redis-connect'
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   console.log('server is working. Port: \x1b[32m%s\x1b[0m', process.env.PORT)
-  void ConnectRedis()
 })

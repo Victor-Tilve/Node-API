a593cefc8fc41f4662811a81064116cb23df4d18
Victor-Tilve <ing.victortilve@gmail.com>
2022-12-18 19:39:52 -0600

    refactor: routes.ts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,6 +4,10 @@ import fg from 'fast-glob'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  // FIXME: what happen when the server is under javaScript
+  fg.sync('src/main/routes/**routes.*')
+    .map(async file => {
+      const route = (await import(`../../../${file}`)).default
+      route(router)
+    })
 }

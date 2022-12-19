17f1ed72b593a970475b11ecd66fa4fb64051f53
Victor-Tilve <ing.victortilve@gmail.com>
2022-12-19 01:04:30 -0600

    feat: caching with redis

--- src/services/IATIDatastoreApi-service.ts
+++ src/services/IATIDatastoreApi-service.ts
@@ -1,6 +1,22 @@
 import { IatiDatastoreApiRepository } from '../domain/repositories/IatiDatastoreApi-repository'
 // import { IIatiDatastoreApiDocs, IQueryParams, MonetaryAidResponse } from '../interfaces/transaction-interface'
-import { IQueryParams, MonetaryAidResponse } from '../interfaces/transaction-interface'
+import { IIatiDatastoreApiResponse, IQueryParams, MonetaryAidResponse } from '../interfaces/transaction-interface'
+import * as redis from 'redis'
+// FIXME: config the time of the caching
+let redisClient
+void (async () => {
+  redisClient = redis.createClient({
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT)
+    },
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    url: process.env.REDIS_URL
+  })
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  redisClient.on('error', (error) => console.error(`Error : ${error}`))
+  await redisClient.connect()
+})()
 
 export class IatiDatastoreApiService {
   iatiDatastoreApiRepository: IatiDatastoreApiRepository
@@ -10,9 +26,29 @@ export class IatiDatastoreApiService {
 
   async getData (params: IQueryParams): Promise<MonetaryAidResponse> {
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions, prefer-const
-    let monetaryAidResponse: MonetaryAidResponse = {}
-    const dataResponse = await this.iatiDatastoreApiRepository.fetchData(params)
-    console.log('IatiDatastoreApiService::getData::dataResponse.response.numFound: ' + String(dataResponse.response?.numFound))
+    let monetaryAidResponse: MonetaryAidResponse
+    let dataResponse: IIatiDatastoreApiResponse
+    try {
+      const countryCode = params.q.replace('recipient_country_code:', '')
+      const cacheResults = await redisClient.get(countryCode)
+      if (cacheResults !== undefined && cacheResults != null && Object.keys(cacheResults).length !== 0) {
+        monetaryAidResponse = JSON.parse(cacheResults)
+      } else {
+        dataResponse = await this.iatiDatastoreApiRepository.fetchData(params)
+        monetaryAidResponse = this.processData(dataResponse)
+        await redisClient.set(countryCode, JSON.stringify(monetaryAidResponse))
+      }
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+      console.log('IatiDatastoreApiService::getData: error: ' + JSON.stringify(error))
+      monetaryAidResponse = {}
+    }
+
+    return monetaryAidResponse
+  }
+
+  private processData (dataResponse: IIatiDatastoreApiResponse): MonetaryAidResponse {
+    const monetaryAidResponse: MonetaryAidResponse = {}
 
     const docs = dataResponse.response?.docs
     if (docs) {
